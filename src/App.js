163d9d13ad983fffab8a82fcc889bd924417935b
AndelijaKaluderovic <andeka011@gmail.com>
2021-04-26 14:55:17 +0200

    Added Weather component and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by
-        <a
-          href="https://www.linkedin.com/in/andjelija-kaludjerovic/"
-          target="_blank"
-        >
-          {" "}
-          Andelija Kaluderovic
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/AndelijaKaluderovic/react-weather-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by
+          <a
+            href="https://www.linkedin.com/in/andjelija-kaludjerovic/"
+            target="_blank"
+          >
+            {" "}
+            Andelija Kaluderovic
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/AndelijaKaluderovic/react-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
